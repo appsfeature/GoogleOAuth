4db4940ae9e3a10bbde3eb103983425d42a4c43f
droidhelios <droidhelios@gmail.com>
2023-02-10 13:05:19 +0530

    Initial commit

--- app/src/main/java/com/simple/googleoauth/MainActivity.java
+++ app/src/main/java/com/simple/googleoauth/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvStatus = findViewById(R.id.tv_status);
 
-        GoogleOAuthSession.setAuthProvider("532271504370-irf3thlpul1n5cpvqnr9868g3eo5790k.apps.googleusercontent.com");
+        GoogleOAuthSession.setAuthProvider("987654321012-frf3thlful1nfcpvqne0668g3eo5780k.apps.googleusercontent.com");
     }
 
     @Override
